f84647cb5887d98b21d6d0db1408961fb0ac1b28
jorda <jorda@LAPTOP-T3PFDD2H>
2022-11-21 19:40:17 -0600

    Incorporando Spring Security

--- src/main/java/com/tienda/entity/Persona.java
+++ src/main/java/com/tienda/entity/Persona.java
@@ -2,6 +2,9 @@
 package com.tienda.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -23,6 +26,11 @@ public class Persona implements Serializable  {
     private String apellido2;
     private String telefono;
     private String email;
+    
+    private String password;
+    private int active;
+    private String roles="";
+    private String permissions="";
     
     //Tenemos estre atributo que es un FK que apunta a la tabla de pais
     @ManyToOne //dice muchos a uno, osea muchas personas pueden ser de un solo pais
@@ -84,7 +92,51 @@ public class Persona implements Serializable  {
     public void setPais(Pais pais) {
         this.pais = pais;
     }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getActive() {
+        return active;
+    }
+
+    public void setActive(int active) {
+        this.active = active;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String permissions) {
+        this.permissions = permissions;
+    }
     
+    public List<String> getRoleList(){
+        if (this.roles.length()>0){
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
     
+    public List<String> getPermissionList(){
+        if (this.permissions.length()>0){
+            return Arrays.asList(this.permissions.split(","));
+        }
+        return new ArrayList<>();
+    }
     
     }
--- src/main/java/com/tienda/repository/PersonaRepository.java
+++ src/main/java/com/tienda/repository/PersonaRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PersonaRepository extends CrudRepository <Persona,Long>{
-
+    Persona findByNombre (String nombre);
     
     
 }
